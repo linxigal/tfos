281c3ed85e7d7e63572137912c2b7a7594c8179e
wwwa <wwwa>
2019-06-21 23:53:49 +0800

    feat: add test_inference.py

--- examples/test_model/test_inference.py
+++ examples/test_model/test_inference.py
@@ -5,3 +5,70 @@
 :Time:  :2019/6/18 15:36
 :File   : test_inference.py
 """
+import json
+import tensorflow as tf
+from tensorflowonspark import TFCluster
+from examples.base import *
+from examples.test_model.worker import BaseWorker
+
+
+class Worker(BaseWorker):
+    def execute_model(self):
+        with tf.Session(self.server.target) as sess:
+            sess.run(tf.global_variables_initializer())
+            self.model.evaluate_generator(generator=self.generate_rdd_data(self.tf_feed),)
+            self.tf_feed.terminate()
+
+
+class TestInferenceModel(Base):
+    def __init__(self, input_rdd_name, input_config, cluster_size, num_ps, batch_size,
+                 epochs,
+                 # steps_per_epoch,
+                 model_dir):
+        super(TestInferenceModel, self).__init__()
+        self.p('input_rdd_name', input_rdd_name)
+        self.p('input_config', input_config)
+        self.p('cluster_size', cluster_size)
+        self.p('num_ps', num_ps)
+        self.p('batch_size', batch_size)
+        self.p('epochs', epochs)
+        # self.p('steps_per_epoch', steps_per_epoch)
+        self.p('model_dir', model_dir)
+
+    def run(self):
+        param = self.params
+
+        # param = json.loads('<#zzjzParam#>')
+        input_rdd_name = param.get('input_rdd_name')
+        input_config = param.get('input_config')
+        cluster_size = param.get('cluster_size')
+        num_ps = param.get('num_ps')
+
+        batch_size = param.get('batch_size')
+        epochs = param.get('epochs')
+        # steps_per_epoch = param.get('steps_per_epoch')
+        model_dir = param.get('model_dir')
+
+        # load data
+        assert input_rdd_name, "parameter input_rdd_name cannot empty!"
+        input_rdd = inputRDD(input_rdd_name)
+        assert input_rdd, "cannot get rdd data from previous input layer!"
+        # print(input_rdd.rdd.take(1))
+        # load config
+        assert input_config, "parameter input_model_config cannot empty!"
+        model_config_rdd = inputRDD(input_config)
+        assert model_config_rdd, "cannot get model config rdd from previous model layer!"
+        columns = model_config_rdd.columns
+        assert "model_config" in columns, "not exists model layer config!"
+        assert "compile_config" in columns, "not exists model compile config!"
+        model_config = json.loads(model_config_rdd.first().model_config)
+        compile_config = json.loads(model_config_rdd.first().compile_config)
+        # print(json.dumps(model_config, indent=4))
+        # print(json.dumps(compile_config, indent=4))
+        n_samples = input_rdd.count()
+        steps_per_epoch = n_samples // batch_size
+        worker = Worker(model_config, compile_config, batch_size, epochs, steps_per_epoch, model_dir)
+        cluster = TFCluster.run(sc, worker, None, cluster_size, num_ps, input_mode=TFCluster.InputMode.SPARK)
+        ouput_rdd = cluster.inference(input_rdd.rdd)
+        ouput_rdd.show()
+        outputRDD('<#zzjzRddName#>', ouput_rdd)
---
+++ examples/test_model/worker.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+:Author  : weijinlong
+:Time    : 
+:File    : 
+"""
+
+import os
+import shutil
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.keras.callbacks import TensorBoard, ModelCheckpoint
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.saved_model import builder as saved_model_builder
+from tensorflow.python.saved_model import tag_constants
+from tensorflow.python.saved_model.signature_def_utils_impl import predict_signature_def
+
+from tensorflowonspark import TFNode
+
+
+class BaseWorker(object):
+    def __init__(self, model_config, compile_config, batch_size, epochs, steps_per_epoch, model_dir):
+        self.compile_config = compile_config
+        self.model_config = model_config
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.steps_per_epoch = steps_per_epoch
+        self.model_dir = model_dir
+        self.task_index = None
+        self.job_name = None
+        self.tf_feed = None
+        self.cluster = None
+        self.server = None
+        self.model = None
+        self.checkpoint_path = os.path.join(self.model_dir, "checkpoint")
+        if not os.path.exists(self.checkpoint_path):
+            os.makedirs(self.checkpoint_path)
+        self.checkpoint_file = os.path.join(self.checkpoint_path, 'model_checkpoint.hdf5')
+        self.tensorboard_path = os.path.join(self.model_dir, "tensorboard")
+        self.export_path = os.path.join(self.model_dir, "export")
+
+    def generate_rdd_data(self, tf_feed):
+        while not tf_feed.should_stop():
+            batch = tf_feed.next_batch(self.batch_size)
+            inputs = []
+            labels = []
+            for item in batch:
+                inputs.append(item.features)
+                labels.append(item.label)
+            inputs = np.array(inputs).astype('float32')
+            labels = np.array(labels).astype('float32')
+            yield (inputs, labels)
+
+    def build_model(self):
+        if self.task_index is None:
+            raise ValueError("task_index cannot None!!!")
+        with tf.device(tf.train.replica_device_setter(
+                worker_device="/job:worker/task:%d" % self.task_index, cluster=self.cluster)):
+            model = Sequential().from_config(self.model_config)
+
+            if self.checkpoint_file and self.task_index == 0:
+                model.load_weights(self.checkpoint_file)
+
+            model.summary()
+            model.compile(**self.compile_config)
+        self.model = model
+        return model
+
+    def execute_model(self):
+        raise NotImplementedError
+
+    def __save_model(self, sess):
+        if self.export_dir and self.job_name == 'worker' and self.task_index == 0:
+            # save a local Keras model, so we can reload it with an inferencing learning_phase
+            if os.path.exists(self.export_dir):
+                shutil.rmtree(self.export_dir)
+
+            # save_model(self.model, "tmp_model")
+            # # reload the model
+            # K.set_learning_phase(False)
+            # new_model = load_model("tmp_model")
+
+            # export a saved_model for inferencing
+            builder = saved_model_builder.SavedModelBuilder(self.export_dir)
+            signature = predict_signature_def(inputs={'images': self.model.input},
+                                              outputs={'scores': self.model.output})
+            builder.add_meta_graph_and_variables(sess=sess,
+                                                 tags=[tag_constants.SERVING],
+                                                 signature_def_map={'predict': signature},
+                                                 clear_devices=True)
+            builder.save()
+
+    def __call__(self, args, ctx):
+        self.task_index = ctx.task_index
+        self.job_name = ctx.job_name
+        self.cluster, self.server = TFNode.start_cluster_server(ctx)
+        self.tf_feed = TFNode.DataFeed(ctx.mgr)
+
+        if ctx.job_name == "ps":
+            self.server.join()
+        elif ctx.job_name == "worker":
+            self.build_model()
+            self.execute_model()
