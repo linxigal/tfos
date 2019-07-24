3b22f608993f885fd128e2a93e2a49959f93bb0b
wwwa <wwwa>
2019-07-23 22:36:37 -0400

    fix: fix operator couldn't execute on deepinsight

--- deep_insight/nets/gans/cgan_mlp.py
+++ deep_insight/nets/gans/cgan_mlp.py
@@ -4,8 +4,19 @@
 :Author : weijinlong
 :Time:  : 2019/7/22 11:18
 :File   : cgan_mlp.py
+
+:content:
+    execute train:
+        spark-submit    --master yarn \
+                        deep_insight/nets/gans/cgan_mlp.py \
+                        --data_dir /home/wjl/github/tfos/data/mnist \
+                        --output_dir /home/wjl/github/tfos/data/results \
+                        --ckpt_dir /home/wjl/github/tfos/data/checkpoint \
+                        --steps 10000 \
+                        --batch_size 32
 """
 
+import argparse
 import os
 import shutil
 
@@ -38,13 +49,22 @@ class CGAN_MLP(Base):
 
 
 if __name__ == "__main__":
-    data_dir = os.path.join(ROOT_PATH, 'data', 'Mnist')
-    output_dir = os.path.join(ROOT_PATH, 'data', 'results')
-    if os.path.exists(output_dir):
-        shutil.rmtree(output_dir)
-    os.makedirs(output_dir)
-    ckpt_dir = os.path.join(ROOT_PATH, 'data', 'checkpoint')
-    if os.path.exists(ckpt_dir):
-        shutil.rmtree(ckpt_dir)
-    os.makedirs(ckpt_dir)
-    CGAN_MLP(data_dir, output_dir, ckpt_dir, 1000000, 32).run()
+    parser = argparse.ArgumentParser(__doc__)
+    model_dir = os.path.join(ROOT_PATH, 'output_data', "model_dir")
+
+    parser.add_argument("--data_dir", help="HDFS path to save/load model during train/inference")
+    parser.add_argument("--output_dir", help="HDFS path to save/load model during train/inference")
+    parser.add_argument("--ckpt_dir", help="HDFS path to save/load model during train/inference")
+    parser.add_argument("--steps", help="number of epochs", type=int, default=10000)
+    parser.add_argument("--batch_size", help="number of records per batch", type=int, default=32)
+    args = parser.parse_args()
+    # data_dir = os.path.join(ROOT_PATH, 'data', 'Mnist')
+    # output_dir = os.path.join(ROOT_PATH, 'data', 'results')
+    # if os.path.exists(output_dir):
+    #     shutil.rmtree(output_dir)
+    # os.makedirs(output_dir)
+    # ckpt_dir = os.path.join(ROOT_PATH, 'data', 'checkpoint')
+    # if os.path.exists(ckpt_dir):
+    #     shutil.rmtree(ckpt_dir)
+    # os.makedirs(ckpt_dir)
+    CGAN_MLP(**args.__dict__).run()
