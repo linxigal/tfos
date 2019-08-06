f73751647d890a05009e864e507bb8af0e5ca8ae
wwwa <wwwa>
2019-08-06 06:03:23 -0400

    new: add convolution.py
    - add Convolution1D,Convolution2D,Convolution3D
    - add Convolution2DTranspose, Convolution3DTranspose
    - add SummaryLayer

--- tfos/layers/convolution.py
+++ tfos/layers/convolution.py
@@ -6,14 +6,112 @@
 :File   : convolution.py
 """
 
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Conv1D, Conv2D, Conv3D, Conv2DTranspose, Conv3DTranspose, ConvLSTM2D
+from tensorflow.python.keras.layers import Conv1D, Conv2D, Conv3D, Conv2DTranspose, Conv3DTranspose
 
-from tfos.base import BaseLayer, get_model_config, ext_exception
+from tfos.base import BaseLayer, ext_exception
 
 
-class Convolution2DLayer(BaseLayer):
+class Conv1DLayer(BaseLayer):
+    @ext_exception("Convolution 1D Layer")
+    def add(self, filters, kernel_size,
+            strides=1,
+            padding='valid',
+            data_format='channels_last',
+            dilation_rate=1,
+            activation=None,
+            use_bias=True, **kwargs):
+        return self._add_layer(Conv1D(filters, kernel_size,
+                                      strides=strides,
+                                      padding=padding,
+                                      data_format=data_format,
+                                      dilation_rate=dilation_rate,
+                                      use_bias=use_bias,
+                                      **kwargs))
 
+    add.__doc__ = Conv2D.__doc__
+
+
+class Conv2DLayer(BaseLayer):
     @ext_exception("Convolution 2D Layer")
-    def add(self, filters, kernel_size):
-        pass
+    def add(self, filters, kernel_size,
+            strides=(1, 1),
+            padding='valid',
+            data_format=None,
+            dilation_rate=(1, 1),
+            activation=None,
+            use_bias=True, **kwargs):
+        return self._add_layer(Conv2D(filters, kernel_size,
+                                      strides=strides,
+                                      padding=padding,
+                                      data_format=data_format,
+                                      dilation_rate=dilation_rate,
+                                      activation=activation,
+                                      use_bias=use_bias,
+                                      **kwargs))
+
+    add.__doc__ = Conv2D.__doc__
+
+
+class Conv3DLayer(BaseLayer):
+    @ext_exception("Convolution 3D Layer")
+    def add(self, filters, kernel_size,
+            strides=(1, 1, 1),
+            padding='valid',
+            data_format=None,
+            dilation_rate=(1, 1, 1),
+            activation=None,
+            use_bias=True, **kwargs):
+        return self._add_layer(Conv3D(filters, kernel_size,
+                                      strides=strides,
+                                      padding=padding,
+                                      data_format=data_format,
+                                      dilation_rate=dilation_rate,
+                                      activation=activation,
+                                      use_bias=use_bias,
+                                      **kwargs))
+
+    add.__doc__ = Conv3D.__doc__
+
+
+class Conv2DTransposeLayer(BaseLayer):
+    @ext_exception("Convolution 2D Transpose Layer")
+    def add(self, filters, kernel_size,
+            strides=(1, 1),
+            padding='valid',
+            output_padding=None,
+            data_format=None,
+            dilation_rate=(1, 1),
+            activation=None,
+            use_bias=True, **kwargs):
+        return self._add_layer(Conv2DTranspose(filters, kernel_size,
+                                               strides=strides,
+                                               padding=padding,
+                                               output_padding=output_padding,
+                                               data_format=data_format,
+                                               dilation_rate=dilation_rate,
+                                               activation=activation,
+                                               use_bias=use_bias,
+                                               **kwargs))
+
+    add.__doc__ = Conv2DTranspose.__doc__
+
+
+class Conv3DTransposeLayer(BaseLayer):
+    @ext_exception("Convolution 3D Transpose Layer")
+    def add(self, filters, kernel_size,
+            strides=(1, 1, 1),
+            padding='valid',
+            output_padding=None,
+            data_format=None,
+            activation=None,
+            use_bias=True, **kwargs):
+        return self._add_layer(Conv3DTranspose(filters, kernel_size,
+                                               strides=strides,
+                                               padding=padding,
+                                               output_padding=output_padding,
+                                               data_format=data_format,
+                                               activation=activation,
+                                               use_bias=use_bias,
+                                               **kwargs))
+
+    add.__doc__ = Conv3DTranspose.__doc__
