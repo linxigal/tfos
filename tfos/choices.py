f29048a02cefd9a348ff589f2c3195d2b2e3dc03
wwwa <you@example.com>
2019-12-24 02:23:32 -0500

    new: fix recurrentPredictModel feature type

--- tfos/choices.py
+++ tfos/choices.py
@@ -9,6 +9,7 @@ from tfos.base.config import *
 
 BOOLEAN = ['true', 'false']
 PADDING = ['valid', 'same']
+FEATURE_TYPE = ['one_hot', 'label_encode']
 OUTPUT_FORMAT = ['json', 'csv']
 MNIST_FORMAT = ['tfr', 'csv', 'pickle', 'gz', 'npz']
 DATA_MODE = ['train', 'test']
